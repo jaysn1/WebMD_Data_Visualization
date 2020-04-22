0c10f0015d2997e006a67aa7c29da320cc38f667
Siddharth Sheladiya <siddharthsheladiya@gmail.com>
2020-04-22 03:43:37 -0700

    adding tooltip for body parts

--- js/bavlo.js
+++ js/bavlo.js
@@ -21,11 +21,24 @@
 //         d3.select(part).style('opacity', 1);
 //       });
 // };
+
+
+var tip = d3.tip()
+            .attr('class', 'd3-tip')
+            .offset([-10, 0])
+            .html(function(d) {
+                    return "<strong>"+d+"</strong> <span style='color:red'></span>";
+                  })
+
 var svg = d3.select("#bavlo")
-      .selectAll("path")
-      .on("mouseover", function(d){
-                  d3.select(this).style("opacity",1);
-            })
-      .on("mouseout", function(d) {
-                  d3.select(this).style("opacity",0.1);
-            });
+			.selectAll("path")
+			.on("mouseover", function(d){
+		          d3.select(this).style("opacity",1);
+		          svg.call(tip);
+		          console.log(this.id);
+		          tip.show(this.id);
+				    })
+			.on("mouseout", function(d) {
+			          d3.select(this).style("opacity",0.1);
+			          tip.hide();
+				    });
